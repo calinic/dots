342c81b8769aff159df4a9b108884d55f416b966
calinic <nnnellum@gmail.com>
2020-01-19 12:51:11 -0800

    polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Terminate already running bars
+killall -q polybar
+
+# Wait until bars have been terminated
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar DP-2 &
+polybar HDMI-0 &
